5a520c4f879199f1650eb271feffe0acb989ec31
Almaz <arzykulov204@gmail.com>
2022-08-09 16:27:21 +0600

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useReducer } from "react";
+import "./App.css";
+import { Post } from "./components/Post";
+// function reducer(counter,action) {
+//   switch (action.type){
+//     case 'increment':
+//       return counter+1
+//       case 'decrement':
+//       return counter-1
+//   }
+// }
+function reducer(state, action) {
+  // switch (action.type) {
+  //   case "plus":
+  //     return state + 5;
+  //     case 'minus':
+  //     return state -5
+  // }
+}
 function App() {
+  // const [counter,dispatch]=useReducer(reducer,0)
+  // const [state, dispatch] = useReducer(reducer, 0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+      {/* <button onClick={()=>dispatch({type:'increment'})}>+</button>
+    <h1>{counter}</h1>
+    
+    <button onClick={()=>dispatch({type:'decrement'})}>-</button> */}
+      {/* <button onClick={() => dispatch({ type: "plus" })}>+</button>
+      <button>{state}</button>
+      <button onClick={()=>dispatch({type:'minus'})}>-</button> */}
+      <Post/>
     </div>
-  );
+  )
 }
 
 export default App;
